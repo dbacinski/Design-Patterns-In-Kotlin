88e0640640de1d78083ace84381254559abcdd93
Dariusz Bacinski <dariusz.bacinski@upnext.io>
2016-09-26 23:47:56 +0200

    Strategy pattern

---
+++ src/main/kotlin/Strategy.kt
@@ -0,0 +1,32 @@
+interface StringFormatter {
+    fun formatString(string: String): String
+}
+
+class Printer(val strategy: StringFormatter) {
+
+    fun printString(string: String) {
+        println(strategy.formatString(string));
+    }
+}
+
+class UpperCaseFormatter : StringFormatter {
+
+    override fun formatString(string: String): String {
+        return string.toUpperCase()
+    }
+}
+
+class LowerCaseFormatter : StringFormatter {
+
+    override fun formatString(string: String): String {
+        return string.toLowerCase()
+    }
+}
+
+fun main(args: Array<String>) {
+    val lowerCasePrinter = Printer(LowerCaseFormatter())
+    lowerCasePrinter.printString("LOREM ipsum DOLOR sit amet")
+
+    val upperCasePrinter = Printer(UpperCaseFormatter())
+    upperCasePrinter.printString("LOREM ipsum DOLOR sit amet")
+}
