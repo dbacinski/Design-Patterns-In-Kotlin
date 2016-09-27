4cea6232e21f46f68f394c4edcbf9a3a108676fb
Dariusz Bacinski <dariusz.bacinski@upnext.io>
2016-09-27 22:54:51 +0200

    Command pattern code

---
+++ src/main/kotlin/Command.kt
@@ -0,0 +1,42 @@
+import java.util.*
+
+interface OrderCommand {
+    fun execute()
+}
+
+class OrderAddCommand(val id: Long) : OrderCommand {
+    override fun execute() {
+        println("adding order with id: $id")
+    }
+}
+
+class OrderPayCommand(val id: Long) : OrderCommand {
+    override fun execute() {
+        println("paying for order with id: $id")
+    }
+}
+
+class CommandProcessor {
+
+    private val queue = ArrayList<OrderCommand>()
+
+    fun addToQueue(orderCommand: OrderCommand): CommandProcessor {
+        queue.add(orderCommand)
+        return this;
+    }
+
+    fun processCommands(): CommandProcessor {
+        queue.forEach { it.execute() }
+        queue.clear()
+        return this;
+    }
+}
+
+fun main(args: Array<String>) {
+    CommandProcessor()
+            .addToQueue(OrderAddCommand(1L))
+            .addToQueue(OrderAddCommand(2L))
+            .addToQueue(OrderPayCommand(2L))
+            .addToQueue(OrderPayCommand(1L))
+            .processCommands()
+}
