01a862d590ac7a87814eec18de6d8e54d1f983e4
Dariusz Bacinski <dariusz.bacinski@upnext.io>
2016-10-07 22:01:58 +0200

    Builder code example

---
+++ src/main/kotlin/Builder.kt
@@ -0,0 +1,49 @@
+import java.io.File
+
+class Dialog() {
+
+    fun showTitle() = println("showing title");
+
+    fun setTitle(title: String) = println("setting title ${title}")
+
+    fun showMessage() = println("showing message");
+
+    fun setMessage(message: String) = println("setting message ${message}")
+
+    fun showImage(bitmapBytes: ByteArray) = println("showing image with size ${bitmapBytes.size}")
+}
+
+class DialogBuilder(var title: String? = null, var message: String? = null, var image: File? = null) {
+
+    fun build(): Dialog {
+        val dialog = Dialog()
+
+        if (title != null) {
+            dialog.setTitle(title!!)
+            dialog.showTitle()
+        }
+
+        if (message != null) {
+            dialog.setMessage(message!!)
+            dialog.showMessage()
+        }
+
+        if (image != null) {
+            dialog.showImage(image!!.readBytes())
+        }
+
+        return dialog
+    }
+}
+
+fun main(args: Array<String>) {
+
+    DialogBuilder()
+            .apply {
+                title = "Dialog Title"
+                message = "Dialog Message"
+                image = File.createTempFile("image", "jpg")
+            }
+            .build()
+}
+
