539f811fa632c272b6c5991e85d48f450ae7b701
Dariusz Bacinski <dariusz.bacinski@upnext.io>
2016-10-11 23:13:30 +0200

    Protection Proxy code example

---
+++ src/main/kotlin/ProtectionProxy.kt
@@ -0,0 +1,32 @@
+interface File {
+    fun read(name: String)
+}
+
+class NormalFile : File {
+    override fun read(name: String) {
+        println("Reading file: $name")
+    }
+}
+
+//Proxy:
+class SecuredFile : File {
+    val normalFile = NormalFile()
+    var password: String = ""
+
+    override fun read(name: String) {
+        if (password == "secret") {
+            println("Password is correct: $password");
+            normalFile.read(name)
+        } else {
+            println("Incorrect password. Access denied!")
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val securedFile = SecuredFile()
+    securedFile.read("readme.md")
+
+    securedFile.password = "secret"
+    securedFile.read("readme.md")
+}
