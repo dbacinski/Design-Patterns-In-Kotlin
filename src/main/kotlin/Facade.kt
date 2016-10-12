8b2dfafbdc5fdfe0b81adbd54fdce52fa2b4d884
Dariusz Bacinski <dariusz.bacinski@upnext.io>
2016-10-12 22:28:36 +0200

    Facade code example

---
+++ src/main/kotlin/Facade.kt
@@ -0,0 +1,46 @@
+import java.util.*
+
+class ComplexSystemStore(val filePath: String) {
+
+    init {
+        println("Reading data from file: $filePath")
+    }
+
+    val store = HashMap<String, String>()
+
+    fun store(key: String, payload: String) {
+        store.put(key, payload)
+    }
+
+    fun read(key: String): String {
+        return store[key] ?: ""
+    }
+
+    fun commit() {
+        println("Storing cached data: $store to file: $filePath");
+    }
+}
+
+data class User(val login: String)
+
+//Facade:
+class UserRepository {
+    val systemPreferences = ComplexSystemStore("/data/default.prefs")
+
+    fun save(user: User) {
+        systemPreferences.store("USER_KEY", user.login)
+        systemPreferences.commit()
+    }
+
+    fun findFirst(): User {
+        return User(systemPreferences.read("USER_KEY"))
+    }
+}
+
+fun main(args: Array<String>) {
+    val userRepository = UserRepository()
+    val user = User("dbacinski")
+    userRepository.save(user)
+    val resultUser = userRepository.findFirst()
+    println("Found stored user: $resultUser")
+}
