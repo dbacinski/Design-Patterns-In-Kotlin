ce57bca9d8ed6491c9781a48daaafa2031468c31
Aaron Triplett <Net-Geek@users.noreply.github.com>
2016-10-13 04:15:16 -0700

    Use object declaration
    Instead of using a companion object and creating a lazy instance, just use an object declaration.

--- src/main/kotlin/Singleton.kt
+++ src/main/kotlin/Singleton.kt
@@ -1,15 +1,13 @@
-class PrinterDriver() {
-    fun print() = println("Printing with object: $this")
-
-    companion object {
-        val instance: PrinterDriver by lazy {
-            PrinterDriver().apply { println("Initializing with object: $this") }
-        }
+object PrinterDriver {
+    init {
+        println("Initializing with object: $this")
     }
+
+    fun print() = println("Printing with object: $this")
 }
 
 fun main(args: Array<String>) {
     println("Start")
-    PrinterDriver.instance.print()
-    PrinterDriver.instance.print()
+    PrinterDriver.print()
+    PrinterDriver.print()
 }
