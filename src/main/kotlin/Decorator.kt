7955c42c0f82300f4756090731074b854ea8f589
Dariusz Bacinski <dariusz.bacinski@upnext.io>
2017-06-24 08:46:01 +0200

    #12 improved version of decorator based on @westonal feedback

--- src/main/kotlin/Decorator.kt
+++ src/main/kotlin/Decorator.kt
@@ -12,16 +12,17 @@ class NormalCoffeeMachine : CoffeeMachine {
 //Decorator:
 class EnhancedCoffeeMachine(val coffeeMachine: CoffeeMachine) : CoffeeMachine by coffeeMachine {
 
+    // overriding behaviour
+    override fun makeLargeCoffee() {
+        println("Enhanced: Making large coffee")
+        coffeeMachine.makeLargeCoffee()
+    }
+
+    // extended behaviour
     fun makeCoffeeWithMilk() {
         println("Enhanced: Making coffee with milk")
         coffeeMachine.makeSmallCoffee()
         println("Enhanced: Adding milk")
-    }
-
-    fun makeDoubleLargeCoffee() {
-        println("Enhanced: Making double large coffee")
-        coffeeMachine.makeLargeCoffee()
-        coffeeMachine.makeLargeCoffee()
     }
 }
 
@@ -29,7 +30,10 @@ fun main(args: Array<String>) {
     val normalMachine = NormalCoffeeMachine()
     val enhancedMachine = EnhancedCoffeeMachine(normalMachine)
 
+    // non-overridden behaviour
+    enhancedMachine.makeSmallCoffee()
+    // overriding behaviour
+    enhancedMachine.makeLargeCoffee()
+    // extended behaviour
     enhancedMachine.makeCoffeeWithMilk()
-
-    enhancedMachine.makeDoubleLargeCoffee()
 }
