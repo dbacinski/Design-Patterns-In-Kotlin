e73e60427091175ef87902c763afe0b703ccf881
Dariusz Bacinski <dariusz.bacinski@gmail.com>
2019-01-11 12:11:51 +0100

    Strategy: migrated to test

--- patterns/src/test/kotlin/Strategy.kt
+++ patterns/src/test/kotlin/Strategy.kt
@@ -1,19 +1,29 @@
-class Printer(val stringFormatterStrategy: (String) -> String) {
-    fun printString(string: String) = println(stringFormatterStrategy.invoke(string))
+import org.junit.jupiter.api.Test
+
+class Printer(private val stringFormatterStrategy: (String) -> String) {
+
+    fun printString(string: String) {
+        println(stringFormatterStrategy(string))
+    }
 }
 
-val lowerCaseFormatter: (String) -> String = { it.toLowerCase() }
+class StrategyTest {
 
-val upperCaseFormatter = { it: String -> it.toUpperCase() }
+    @Test
+    fun `Strategy`() {
+        val lowerCaseFormatter: (String) -> String = { it.toLowerCase() }
+        val upperCaseFormatter = { it: String -> it.toUpperCase() }
 
-fun main(args: Array<String>) {
+        val inputString = "LOREM ipsum DOLOR sit amet"
 
-    val lowerCasePrinter = Printer(lowerCaseFormatter)
-    lowerCasePrinter.printString("LOREM ipsum DOLOR sit amet")
+        val lowerCasePrinter = Printer(lowerCaseFormatter)
+        lowerCasePrinter.printString(inputString)
 
-    val upperCasePrinter = Printer(upperCaseFormatter)
-    upperCasePrinter.printString("LOREM ipsum DOLOR sit amet")
+        val upperCasePrinter = Printer(upperCaseFormatter)
+        upperCasePrinter.printString(inputString)
 
-    val prefixPrinter = Printer({ "Prefix: " + it })
-    prefixPrinter.printString("LOREM ipsum DOLOR sit amet")
+        val prefixPrinter = Printer { "Prefix: $it" }
+        prefixPrinter.printString(inputString)
+    }
 }
+
