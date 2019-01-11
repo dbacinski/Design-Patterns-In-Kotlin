d584e654767a38949267a24722903e6f6a4d1a1d
Dariusz Bacinski <dariusz.bacinski@gmail.com>
2019-01-11 11:50:10 +0100

    Singleton: migrated to test

--- patterns/src/test/kotlin/Singleton.kt
+++ patterns/src/test/kotlin/Singleton.kt
@@ -1,13 +1,24 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 object PrinterDriver {
     init {
         println("Initializing with object: $this")
     }
 
-    fun print() = println("Printing with object: $this")
+    fun print(): PrinterDriver =
+        apply { println("Printing with object: $this") }
 }
 
-fun main(args: Array<String>) {
-    println("Start")
-    PrinterDriver.print()
-    PrinterDriver.print()
+class SingletonTest {
+
+    @Test
+    fun `Singleton`() {
+        println("Start")
+        val printerFirst = PrinterDriver.print()
+        val printerSecond = PrinterDriver.print()
+
+        assertThat(printerFirst).isSameAs(PrinterDriver)
+        assertThat(printerSecond).isSameAs(PrinterDriver)
+    }
 }
