a09b3c7c8f1f364e0a4e164147f6e00d870b5dba
Dariusz Bacinski <dariusz.bacinski@gmail.com>
2019-09-03 11:19:56 +0200

    Visitor: formatting and update README

--- patterns/src/test/kotlin/Visitor.kt
+++ patterns/src/test/kotlin/Visitor.kt
@@ -7,7 +7,6 @@ interface ReportVisitable {
 
 class FixedPriceContract(val costPerYear: Long) : ReportVisitable {
     override fun <R> accept(visitor: ReportVisitor<R>): R = visitor.visit(this)
-
 }
 
 class TimeAndMaterialsContract(val costPerHour: Long, val hours: Long) : ReportVisitable {
@@ -23,27 +22,30 @@ interface ReportVisitor<out R> {
     fun visit(contract: FixedPriceContract): R
     fun visit(contract: TimeAndMaterialsContract): R
     fun visit(contract: SupportContract): R
-
 }
 
 class MonthlyCostReportVisitor : ReportVisitor<Long> {
 
-    override fun visit(contract: FixedPriceContract): Long = contract.costPerYear / 12
+    override fun visit(contract: FixedPriceContract): Long =
+        contract.costPerYear / 12
 
-    override fun visit(contract: TimeAndMaterialsContract): Long = contract.costPerHour * contract.hours
+    override fun visit(contract: TimeAndMaterialsContract): Long =
+        contract.costPerHour * contract.hours
 
-    override fun visit(contract: SupportContract): Long = contract.costPerMonth
-
+    override fun visit(contract: SupportContract): Long =
+        contract.costPerMonth
 }
 
 class YearlyReportVisitor : ReportVisitor<Long> {
 
-    override fun visit(contract: FixedPriceContract): Long = contract.costPerYear
+    override fun visit(contract: FixedPriceContract): Long =
+        contract.costPerYear
 
-    override fun visit(contract: TimeAndMaterialsContract): Long = contract.costPerHour * contract.hours
+    override fun visit(contract: TimeAndMaterialsContract): Long =
+        contract.costPerHour * contract.hours
 
-    override fun visit(contract: SupportContract): Long = contract.costPerMonth * 12
-
+    override fun visit(contract: SupportContract): Long =
+        contract.costPerMonth * 12
 }
 
 class VisitorTest {
@@ -60,14 +62,14 @@ class VisitorTest {
         val monthlyCostReportVisitor = MonthlyCostReportVisitor()
 
         val monthlyCost = projects.map { it.accept(monthlyCostReportVisitor) }
-                .sum()
-        println("Monthly cost: ${monthlyCost}")
+            .sum()
+        println("Monthly cost: $monthlyCost")
         assertThat(monthlyCost).isEqualTo(5333)
 
         val yearlyReportVisitor = YearlyReportVisitor()
         val yearlyCost = projects.map { it.accept(yearlyReportVisitor) }
-                .sum()
-        println("Yearly cost: ${yearlyCost}")
+            .sum()
+        println("Yearly cost: $yearlyCost")
         assertThat(yearlyCost).isEqualTo(20000)
     }
 }
